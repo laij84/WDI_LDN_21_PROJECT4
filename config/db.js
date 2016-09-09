ecb28bd4adb079e98d22651ff824af2788536af0
Jason Lai <jase_lai@hotmail.com>
2016-09-09 11:32:42 +0100

    mongo db update

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 dbURIs = {
   test: "mongodb://localhost/homeworkapi-test",
   development: "mongodb://localhost/homeworkapi",
-  production: process.env.MONGOLAB_URI || "mongodb://localhost/homeworkapi"
+  production: process.env.MONGODB_URI || "mongodb://localhost/homeworkapi"
 }
 
 module.exports = function(env) {
